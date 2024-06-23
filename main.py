7d233512143584a695244837ce779a98f3ed9705
abdulelah-alzoman <abdulelahalzoman@gmail.com>
2024-06-24 01:09:41 +0300

    Major update - pdf support added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import PyPDF2
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 import requests
@@ -15,7 +16,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 
-llm = ChatOpenAI(model='gpt-4-turbo')
+llm = ChatOpenAI(model='gpt-4o')
 memory = ConversationBufferMemory(
     memory_key="chat_history", input_key="human_input")
 app = Flask(__name__)
@@ -58,71 +59,50 @@ def get_data():
 def init_qa_system(doc_path):
     global chain, docsearch
 
-    doc = docx.Document(doc_path)
     text = ''
-    for para in doc.paragraphs:
-        text += para.text + '\n'
+    if doc_path.lower().endswith('.docx'):
+        doc = docx.Document(doc_path)
+        for para in doc.paragraphs:
+            text += para.text + '\n'
+    elif doc_path.lower().endswith('.pdf'):
+        with open(doc_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            for page_num in range(len(reader.pages)):
+                page = reader.pages[page_num]
+                text += page.extract_text() + '\n'
+    else:
+        raise ValueError(
+            "Unsupported file format. Please provide a DOCX or PDF file.")
 
-    # split into chunks
+    # Split into chunks
     char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,
                                                chunk_overlap=200, length_function=len)
-
     text_chunks = char_text_splitter.split_text(text)
 
-    template = """انت مساعد ذكي تتحدث مع انسان، اسمك قوي.
-يجب ان يكون ردك واضح وسهل الفهم
-ردك يجب ان لايتعدى ثلاث جمل
-لاتعطي تفاصيل ان لم يطلبها المستخدم
-اعتمد على نظام العمل السعودي كمرجع اساسي في تقديم الاستشارات.
-اجعل اجاباتك مختصرة قدر الامكان وانهها برقم المادة التي استندت اليها واجعلها بين قوسين 
-لا تستخدم كلمت نظام العمل السعودي لأن كل الاستشارات تقوم على هذا المبدأ 
+    template = """
+    انت مساعد ذكي تتحدث مع انسان، اسمك قوي
+مرفق إليك نظام العمل السعودي يجب ان تكون كل إجاباتك منه
+لا تستخدم كلمت نظام العمل السعودي لأن كل الاستشارات تقوم على هذا المبدأ
 اطلب من المستخدم ان يعطيك جميع المعلومات التي تحتاجها للإجابة بطريقة نموذجية
-اعط اجابه قصيرة ومختصرة قدر الامكان ولا تفصل الا اذا طلب المستخدم ذلك 
-اهم الابواب التي يجب التركيز عليها (5,6,8,15) و اهم المواد التي يجب التركيز عليها ( 55 ، 53 ، 56 ، 66 ، 67 ، 68 ، 69 ، 70 ، 71 ، 74 ،  75 ، 76 ، 77 ، 80 ، 81 ، 82 ، 84 ، 85 ، 88 ، 111 ،117) وهذا لا يعني ان باقي المواد اقل اهميه
-حقوق الموظف في القطاع الخاص 
-التأمين الطبي م. ١٤٤
-راتب الاجازة م.١٠٩
-ساعات العمل الاضافي م. ١٠٧
-دفع الاجور م. ٩٠
-مكافأة نهاية الخدمة م. ٨٤ و٨٥
-رصيد الاجازة التي لم تستخدم م.١١١
-يوم راحة في الاسبوع م.١٠٤
-حقوق الموظف عند ترك العمل 
-
-
-في حال الاستقالة (مكافأة نهاية الخدمة م. 85)
-في حال انتهاء العقد او الانهاء من جانب صاحب العمل (م. 84)
-مستحقات الاجازة التي لم تستخدم (م. 111)
-شهادة الخدمة (م. 64)
-المدة المسموح بها في صرف مكافأة نهاية الخدمة (م. 88)
-إذا وقعت ايام الاجازة المرضية أثناء الاجازة السنوية فتوقف أيام الاجازة السنوية الى حين انتهاء الاجازة المرضية ثم تستأنف المدة المتبقية 
-بشرط ان يكون لديك تقرير طبي بالإجازة المرضية
-
-المستند النظامي المادة 26 من اللائحة التنفيذية لنظام العمل
-يحق للموظف تقديم اجازة ولا يجوز لصاحب العمل رفضها 
-١. اجازة المولود
-٢. الزواج 
-٣. الوفاة 
-٤. الحج 
-٥. الاختبارات 
-٦. المرضية 
-٧. الوضع 
-٨.  العدة
-
-معطى لك أجزاء من مستند طويل (نظام العمل السعودي) وسؤال، اصنع الإجابة النهائية.
-
+اجعل اجاباتك مختصرة قدر الامكان وانهها برقم المادة التي استندت اليها واجعلها بين قوسين
+لا تعطي تفاصيل ان لم يطلبها المستخدم
+يجب ان يكون ردك واضح وسهل الفهم وأن لايتعدى الثلاث 
+الجمل
+اذا سؤلت خارج اطار نظام العمل السعودي اجب لا استطيع مساعدتك في ذلك، ارجوا ان تسألني عن نظام العمل السعودي
+                معطى لك أجزاء من مستند طويل (نظام العمل السعودي) وسؤال، اصنع الإجابة النهائية.
 
                 {context}
 
                 {chat_history}
                 Human: {human_input}
-                Chatbot:"""
+                Chatbot:
+                """
 
     prompt = PromptTemplate(
         input_variables=["chat_history", "human_input", "context"], template=template
     )
 
-    # create embeddings
+    # Create embeddings
     embeddings = OpenAIEmbeddings()
     docsearch = FAISS.from_texts(text_chunks, embeddings)
 
@@ -131,6 +111,6 @@ def init_qa_system(doc_path):
 
 
 if __name__ == '__main__':
-    doc_path = './trainingData/laborLaw.docx'  # Replace with the actual path
+    doc_path = './trainingData/hrsd.pdf'  # Replace with the actual path
     init_qa_system(doc_path)
     app.run(host='0.0.0.0', port=4000)
